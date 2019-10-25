a531c4eefe0bc17f52b8be5f1a1983aa87d471c0
Pierre-Nicolas <pc260533@gr10-25.esirem-ad.ad.u-bourgogne.fr>
2019-10-25 08:58:39 +0200

    Q1 : Ajout de la fonction main.cpp

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "chambre.h"
+#include "client.h"
+ #include "date.h"
+#include "hotel.h"
+
+using namespace std;
+
+int main() {
+    /* Question 1
+    Date date(28, 2, 2019);
+    Date dateBissextile(28, 2, 2020);
+    cout << "La date est de : " << date.toString() << endl;
+    cout << "La date bissextile est de : " << dateBissextile.toString() << endl;
+    date.jourProchain();
+    dateBissextile.jourProchain();
+    cout << "La date est de : " << date.toString() << endl;
+    cout << "La date bissextile est de : " << dateBissextile.toString() << endl;
+    */
+    return 0;
+}
