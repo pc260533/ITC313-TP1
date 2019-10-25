97f5bd59070f607e99723f9ee9df7f9bcf88a39a
Pierre-Nicolas <pc260533@gr10-25.esirem-ad.ad.u-bourgogne.fr>
2019-10-25 09:04:31 +0200

    Q2 et Q3 : Modification de la fonction main.cpp

--- main.cpp
+++ main.cpp
@@ -18,5 +18,19 @@ int main() {
     cout << "La date est de : " << date.toString() << endl;
     cout << "La date bissextile est de : " << dateBissextile.toString() << endl;
     */
+
+    /* Question 2
+    Client client("1", "Chassagne", "Pierre-Nicolas", 0);
+    cout << "La client est : " << client.toString() << endl;
+    client.setNomClient("Polybe");
+    client.setPrenomClient("Polybe");
+    client.ajouterReservation();
+    cout << "La client est : " << client.toString() << endl;
+    */
+
+    /* Question 3
+    Chambre chambre(1,TypeDeChambre::Simple, 10.0);
+    cout << "La chambre est : " << chambre.toString() << endl;
+    */
     return 0;
 }
